e2713cf9e0f6917d75742e8ebf2a601473ea10b0
Pavel Nosovets <pavel.nosovets@codefresh.io>
2020-03-27 11:36:12 +0300

    Created new branch saas-5604

--- lib/vars.rb
+++ lib/vars.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 CHARTS = [
-  { chart: 'stable/prometheus-operator', version: '8.5.14', release: 'cprom',
+  { chart: 'stable/prometheus-operator', version: '8.12.3', release: 'cprom',
     values: 'kube-prometheus.yaml', template: 'kube-prometheus.yaml.erb' },
 ].freeze
 
